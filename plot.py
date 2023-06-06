2604b1feb5a2dba5c198dc14e6ac4b6e7028f0bf
TTC-CCF <93956496+TTC-CCF@users.noreply.github.com>
2023-06-07 02:22:16 +0800

    Finish: fix plot bug

--- plot.py
+++ plot.py
@@ -1,14 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plotAccuracy(title, arr1, arr2, file):
+def plotAccuracy(title, arr1, arr2, file, accuorloss):
     arr1 = np.load(arr1)
     arr2 = np.load(arr2)
 
     plt.figure(figsize=(10, 5))
     plt.title(title)
     plt.xlabel('epoch')
-    plt.ylabel('loss')
+    plt.ylabel(accuorloss)
     plt.plot([i for i in range(len(arr1))], arr1, label='aug')
     plt.plot([i for i in range(len(arr2))], arr2, label='unaug')
     plt.legend()
@@ -30,9 +30,9 @@ if __name__ == '__main__':
     accu3 = './accu/aug_sent.npy'
     accu4 = './accu/unaug_sent.npy'
     
-    plotAccuracy('Teams classification loss', loss1, loss2, './plot/loss_teams.png')
-    plotAccuracy('Sentimental analysis loss', loss3, loss4, './plot/loss_sent.png')
+    plotAccuracy('Teams classification loss', loss1, loss2, './plot/loss_teams.png', 'loss')
+    plotAccuracy('Sentimental analysis loss', loss3, loss4, './plot/loss_sent.png', 'loss')
 
-    plotAccuracy('Teams classification Accuracy', accu1, accu2, './plot/accu_teams.png')
-    plotAccuracy('Sentimental analysis Accuracy', accu3, accu4, './plot/accu_sent.png')    
+    plotAccuracy('Teams classification Accuracy', accu1, accu2, './plot/accu_teams.png', 'Accuracy')
+    plotAccuracy('Sentimental analysis Accuracy', accu3, accu4, './plot/accu_sent.png', 'Accuracy')    
     
